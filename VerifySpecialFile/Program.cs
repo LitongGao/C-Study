c1644c65e1866082aaf08acdd24cf5265e7cf016
gaolitong <gao_litong1984@hotmail.com>
2017-03-15 15:28:08 +0800

    add a method to verify if file is exist

---
+++ VerifySpecialFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VerifySpecialFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VerifySpecialFileIsExist("Pearl.Plugins.Sync.dl");
+            Console.ReadKey();
+        }
+
+        public static bool VerifySpecialFileIsExist(string fileName)
+        {
+            string filePath = @"C:\Intuit\Demandforce\Sync Client\DFLinkClient";
+            string fileNamePath = Path.Combine(filePath, fileName);
+            if (!File.Exists(fileNamePath))
+            {
+                Console.WriteLine("The test file [{0}] does not exist", fileName);
+                return false;
+            }
+            Console.WriteLine("The test file [{0}] does exist", fileName);
+            return true;
+        }
+    }
+}
