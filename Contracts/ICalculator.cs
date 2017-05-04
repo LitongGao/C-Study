dfa7cc17f66cf9e5dd1a9ae4b8d1a34f23d9c4e6
gaolitong <gao_litong1984@hotmail.com>
2017-05-04 17:45:03 +0800

    Add WCF study

---
+++ Contracts/ICalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServices.Contracts
+{
+    [ServiceContract(Name = "CalculatorService", Namespace = "http://www.cnblogs.com/artech/archive/2007/02/26/656901.html")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double x, double y);
+
+        [OperationContract]
+        double Subtract(double x, double y);
+
+        [OperationContract]
+        double Multiply(double x, double y);
+
+        [OperationContract]
+        double Divide(double x, double y);
+    }
+}
